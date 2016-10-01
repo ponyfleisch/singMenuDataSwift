1fa625a7a5e5a40368a1cca59a8bcda01ddd0c32
Claudio Mettler <github@ponyfleisch.ch>
2016-10-02 02:43:53 +0800

    Initial Commit

---
+++ SingData/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SingData
+//
+//  Created by Claudio Mettler on 02.10.16.
+//  Copyright © 2016 Claudio Mettler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
